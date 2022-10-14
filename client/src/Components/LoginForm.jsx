5a5a40ca30f2f5ea531e5972697151f999935ffb
Samiya Kazi <samiya.kazi09@gmail.com>
2022-10-14 18:05:54 +0600

    added apiclientservice login

--- client/src/Components/LoginForm.jsx
+++ client/src/Components/LoginForm.jsx
@@ -2,8 +2,34 @@ import React from "react";
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { Button } from "@mui/material";
+import { useState } from "react";
+import { login } from "../services/apiClientService";
 
 function LoginForm () {
+  const initialState = {
+    email: '',
+    password: '',
+  };
+
+  const [ state, setState ] = useState(initialState);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setState(prevState => ({...prevState, [name]: value}));
+  }
+
+
+  const handleSubmit = () => {
+    async function loginUser (data) {
+      const result = await login(data);
+
+      localStorage.setItem('accessToken', result.headers.authorization);
+    }
+
+    loginUser(state);
+  }
+
+
   return (
     <>
       <div className='form-container'>
@@ -28,15 +54,19 @@ function LoginForm () {
           required
           id="email"
           label="Email"
+          name='email'
+          onChange={handleChange}
         />
          <TextField
           id="password"
           label="Password"
           type="password"
           autoComplete="current-password"
+          name='password'
+          onChange={handleChange}
         />
 
-        <Button variant='contained' color="secondary" >Login</Button>
+        <Button type='submit' variant='contained' color="secondary" onClick={handleSubmit}>Login</Button>
       </div>
     </>
   )
---
+++ client/src/services/apiClientService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const URL = 'http://localhost:3001';
+
+axios.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) req.headers.Authorization = token;
+    return req;
+  },
+  (e) => {
+    return Promise.reject(e);
+  }
+);
+
+export const login = (data) => axios.post(`${URL}/login`, data);
+
+export const signUp = (data) => axios.post(`${URL}/register`, data);
